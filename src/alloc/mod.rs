4947d9b8f54bbbca9b4dd49a47c6e280c04091f7
Bob McWhirter <bmcwhirt@redhat.com>
2020-12-10 15:08:45 -0500

    Bring in the cortex-m allocator without requiring Alloc.

--- src/alloc/mod.rs
+++ src/alloc/mod.rs
@@ -1,3 +1,12 @@
+
+#[cfg(feature="cortex-m")]
+mod cortex_m;
+
+#[cfg(feature="cortex-m")]
+pub(crate) use self::cortex_m::CortexMHeap as Alloc;
+
+#[cfg(feature = "std")]
 mod static_alloc;
 
-pub(crate) use static_alloc::StaticAlloc as Alloc;
+#[cfg(feature = "std")]
+pub(crate) use self::static_alloc::StaticAlloc as Alloc;
